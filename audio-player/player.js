38971b75391b987361d24e3edd97a9d996a4312b
onethousandfifty <onethousandfifty@gmail.com>
2023-03-14 04:54:12 +0100

    commit

--- audio-player/player.js
+++ audio-player/player.js
@@ -1,11 +1,11 @@
-// declare variables using let or const
+// Use const for variables that don't get reassigned and let for variables that do
 const player = document.getElementById("player");
 const progress = document.getElementById("progress");
-const playbtn = document.getElementById("playbtn");
+const playBtn = document.getElementById("playbtn");
 const tracks = document.querySelectorAll(".track");
 let currentTrackIndex = 0;
 
-// play a track
+// Play a track
 function playTrack(index) {
   const track = tracks[index];
   const src = track.getAttribute("data-src");
@@ -13,74 +13,56 @@ function playTrack(index) {
   player.play();
   currentTrackIndex = index;
   setTrackActive(index);
-
-  // update track name in player
-  const trackName = track.innerText;
-  document.querySelector(".info").innerText = trackName;
+  updateTrackName(track.innerText);
 }
 
-// set active track
+// Set active track
 function setTrackActive(index) {
-  tracks.forEach(function (track, i) {
-    if (i === index) {
-      track.classList.add("active");
-    } else {
-      track.classList.remove("active");
-    }
+  tracks.forEach((track, i) => {
+    track.classList.toggle("active", i === index);
   });
 }
 
-// play or pause the player
-function playpause() {
-  if (player.paused) {
-    player.play();
-  } else {
-    player.pause();
-  }
+// Play or pause the player
+function togglePlayPause() {
+  player.paused ? player.play() : player.pause();
 }
 
-// add event listener to play/pause button
-playbtn.addEventListener("click", playpause);
+// Update play/pause button icon
+function updatePlayBtnIcon() {
+  playBtn.classList.toggle("fa-play", player.paused);
+  playBtn.classList.toggle("fa-pause", !player.paused);
+}
 
-// change icon when player is played or paused
-player.onplay = function () {
-  playbtn.classList.remove("fa-play");
-  playbtn.classList.add("fa-pause");
-};
+// Update track name in player
+function updateTrackName(name) {
+  document.querySelector(".info").textContent = name;
+}
 
-player.onpause = function () {
-  playbtn.classList.add("fa-play");
-  playbtn.classList.remove("fa-pause");
-};
-
-// update progress bar and current time
-player.ontimeupdate = function () {
+// Update progress bar and current time
+function updateProgressBar() {
   const ct = player.currentTime;
-  current.innerHTML = timeFormat(ct);
-  // update progress
   const duration = player.duration;
-  const prog = Math.floor((ct * 100) / duration);
-  progress.style.setProperty("--progress", prog + "%");
-};
-
-// format time to mm:ss
-function timeFormat(ct) {
-  let minutes = Math.floor(ct / 60);
-  let seconds = Math.floor(ct % 60);
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  return minutes + ":" + seconds;
+  const prog = Math.floor((ct / duration) * 100);
+  progress.style.setProperty("--progress", `${prog}%`);
+  current.textContent = formatTime(ct);
 }
 
-// add event listener to each track
-tracks.forEach(function (track, i) {
-  track.addEventListener("click", function () {
-    playTrack(i);
-  });
+// Format time to mm:ss
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60).toString().padStart(2, "0");
+  return `${minutes}:${seconds}`;
+}
+
+// Add event listeners
+playBtn.addEventListener("click", togglePlayPause);
+player.addEventListener("play", updatePlayBtnIcon);
+player.addEventListener("pause", updatePlayBtnIcon);
+player.addEventListener("timeupdate", updateProgressBar);
+tracks.forEach((track, i) => {
+  track.addEventListener("click", () => playTrack(i));
 });
 
-// play the first track by default
+// Play the first track by default
 playTrack(currentTrackIndex);
